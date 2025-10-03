990057d6b4859f824b769f1dab79595b36a41013
Jesse Na <=>
2025-10-03 10:19:43 -0400

    create supabase connector in supabase.web.ts, create auth context hook and provider, and add database types file

---
+++ ece1778-app/hooks/use-auth-context.ts
@@ -0,0 +1,19 @@
+import { Session } from "@supabase/supabase-js";
+import { createContext, useContext } from "react";
+import { Tables } from "../database.types";
+
+export type AuthData = {
+  session?: Session | null;
+  profile?: Tables<"profiles"> | null;
+  isLoading: boolean;
+  isLoggedIn: boolean;
+};
+
+export const AuthContext = createContext<AuthData>({
+  session: undefined,
+  profile: undefined,
+  isLoading: true,
+  isLoggedIn: false,
+});
+
+export const useAuthContext = () => useContext(AuthContext);
