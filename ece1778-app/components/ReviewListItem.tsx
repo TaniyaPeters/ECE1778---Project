98209f4a30453f6dcefd79c2ff5eaf94ab1378c1
niloferh <nilofer.hyder@mail.utoronto.ca>
2025-10-08 15:24:18 -0400

    Media details screen

---
+++ ece1778-app/components/ReviewListItem.tsx
@@ -0,0 +1,46 @@
+import { View, Text, StyleSheet } from "react-native";
+import { Review } from "../types";
+import Card from "./Card";
+import { colors } from "../constants/colors";
+import { globalStyles } from "../styles/globalStyles";
+
+type Props = {
+  review: Review;
+};
+
+export default function ReviewListItem({ review }: Props) {
+    return (
+        <Card style={styles.card}>
+            <View style={styles.content}>
+                {/* User who wrote review */}
+                <Text style={[globalStyles.paragraph, styles.text]}>{review.user}</Text>
+                {/* Divider line */}
+                <View style={styles.line}/>
+                {/* Review text */}
+                <Text style={[globalStyles.paragraph, styles.text]}>{review.text}</Text>
+            </View>
+        </Card>
+    );
+}
+
+const styles = StyleSheet.create({
+  card: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  content: {
+    flex: 1,
+    flexDirection: "column",
+  },
+  text: {
+    fontSize: 17,
+    marginVertical: 3,
+  },
+  line: {
+    height: 1, 
+    backgroundColor: colors.text, 
+    width: '100%', 
+    marginVertical: 10, 
+  },
+});
---
+++ ece1778-app/styles/globalStyles.ts
@@ -0,0 +1,46 @@
+import { Platform, StyleSheet } from "react-native";
+import { colors } from "../constants/colors";
+
+const defaultSerif = Platform.OS === 'ios' ? 'Times New Roman' : 'serif';
+const defaultSans = Platform.OS === 'ios' ? 'Helvetica' : 'sans-serif';
+
+export const globalStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: colors.background,
+  },
+  titleText: {
+    fontSize: 40,
+    fontWeight: "bold",
+    color: colors.titleText,
+    textAlign: "center",
+    fontFamily: defaultSerif,
+  },
+  paragraph: {
+    fontSize: 20,
+    color: colors.text,
+    marginVertical: 8,
+    fontFamily: defaultSans,
+  },
+  paragraphBold: {
+    fontSize: 20,
+    color: colors.text,
+    marginVertical: 8,
+    fontWeight: "bold",
+    fontFamily: defaultSans,
+  },
+  detailsImage: {
+    width: '100%',
+    height: 400,
+    aspectRatio: 1,
+    alignSelf: 'center',
+    paddingBottom: 10,
+    paddingTop: 10,
+    borderRadius: 16,
+    shadowColor: "#000",
+    shadowOpacity: 0.55,
+    shadowRadius: 8,
+    shadowOffset: { width: 0, height: 4 },
+  },
+});
