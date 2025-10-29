c4cdf26557ae8794188552ec732981bc626abccf
TaniyaPeters <tpeterratnaraj@gmail.com>
2025-10-29 12:12:51 -0400

    Created a Monthly Notification Component and added it to the Home page. Monthly Component also has a newly creeated Carousel component.

--- ece1778-app/app/(tabs)/(home)/(top-tabs)/_layout.tsx
+++ ece1778-app/app/(tabs)/(home)/(top-tabs)/_layout.tsx
@@ -7,6 +7,8 @@ import { withLayoutContext } from "expo-router";
 import { ParamListBase, TabNavigationState } from "@react-navigation/native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { globalStyles } from "@app/styles/globalStyles";
+import MonthlyRecap from "@app/components/MonthlyRecap";
+import { ScrollView } from "react-native";
 
 const { Navigator } = createMaterialTopTabNavigator();
 
@@ -21,6 +23,7 @@ export default function TopTabsLayout() {
   return (
     <SafeAreaProvider>
       <MaterialTopTabs>
+        <MaterialTopTabs.Screen name="index" options={{ title: "All" }} />
         <MaterialTopTabs.Screen name="movies" options={{ title: "Movies Tab" }} />
         <MaterialTopTabs.Screen name="books" options={{ title: "Books Tab" }} />
       </MaterialTopTabs>
--- ece1778-app/app/(tabs)/(home)/(top-tabs)/movies.tsx
+++ ece1778-app/app/(tabs)/(home)/(top-tabs)/movies.tsx
@@ -1,12 +1,14 @@
+import MonthlyRecap from "@app/components/MonthlyRecap";
 import { globalStyles } from "@app/styles/globalStyles";
 import { router } from "expo-router";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, ScrollView, StyleSheet, Text, View } from "react-native";
 
 export default function TabMovies() {
   return (
-    <View style={globalStyles.container}>
+    <ScrollView style={globalStyles.container}>
       <Text style={globalStyles.titleText}>Movies Tab</Text>
       <Button title="Go to Movie Details" onPress={() => router.push("../movieDetails/1")}/>
-    </View>
+      <MonthlyRecap user="User" type="Movie" action="Watched"></MonthlyRecap>
+    </ScrollView>
   );
 }
