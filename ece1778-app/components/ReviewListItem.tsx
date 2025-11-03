6d6e12c68c707d57c4e83c67b292daf916e9fb7e
niloferh <nilofer.hyder@mail.utoronto.ca>
2025-11-02 19:40:58 -0500

    Updated movie details screen to use data from db instead of hardcoded values

--- ece1778-app/components/ReviewListItem.tsx
+++ ece1778-app/components/ReviewListItem.tsx
@@ -1,6 +1,7 @@
 import { View, Text, StyleSheet } from "react-native";
 import { Review } from "../types/types";
 import Card from "./Card";
+import StarRating from "@components/starRating";
 import { colors } from "../constants/colors";
 import { globalStyles } from "../styles/globalStyles";
 
@@ -12,15 +13,21 @@ export default function ReviewListItem({ review }: Props) {
 	return (
 		<Card style={styles.card}>
 			<View style={styles.content}>
-				{/* User who wrote review */}
-				<Text style={[globalStyles.paragraph, styles.text]}>
-					{review.user}
-				</Text>
+				<View style={styles.headerRow}>
+					{/* User who wrote review */}
+					<Text style={[globalStyles.paragraph, styles.text]}>
+						{review.username}
+					</Text>
+					{/* Rating associated to the review depicted out of 5 stars */}
+					{review.rating && (
+						<StarRating rating={review.rating} color={colors.light.background} />
+					)}
+				</View>
 				{/* Divider line */}
 				<View style={styles.line} />
 				{/* Review text */}
 				<Text style={[globalStyles.paragraph, styles.text]}>
-					{review.text}
+					{review.review}
 				</Text>
 			</View>
 		</Card>
@@ -36,6 +43,11 @@ const styles = StyleSheet.create({
 	content: {
 		flex: 1,
 		flexDirection: "column",
+	},
+	headerRow: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center'
 	},
 	text: {
 		fontSize: 17,
