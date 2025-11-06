8e5575846ffb1205b16fced196a3898527c2b1df
Nagham Sabbour <nagham.sabbour@gmail.com>
2025-11-05 19:47:37 -0500

    #21 Working collection details screen from the profile page. Displays all the movies in the movies tab.

---
+++ ece1778-app/handle_new_user.sql
@@ -0,0 +1,9 @@
+begin
+  insert into public.profiles (id, full_name, avatar_url, email, username)
+  values (new.id, new.raw_user_meta_data->>'full_name', new.raw_user_meta_data->>'avatar_url', new.email,
+    (case when (new.raw_user_meta_data->>'iss' is null) then new.raw_user_meta_data->>'username'
+          when (new.raw_user_meta_data->>'iss' = 'https://api.github.com') then new.raw_user_meta_data->>'user_name'
+          when (new.raw_user_meta_data->>'iss' = 'https://discord.com/api') then new.raw_user_meta_data->>'full_name'
+          else new.raw_user_meta_data->>'username' END));
+  return new;
+end;
